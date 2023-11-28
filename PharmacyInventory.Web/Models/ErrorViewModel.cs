75d7b8fce73402c39676a6068f3eb492a1e277c5
ArteMaxL <artemiolucero@gmail.com>
2023-11-27 23:21:43 -0300

    first commit

---
+++ PharmacyInventory.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PharmacyInventory.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
